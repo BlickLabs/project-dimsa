ac28bf20ee7a6d3481c1851978dab778cf788092
Raúl Marty <santor3medio@gmail.com>
2018-02-03 00:25:34 -0600

    #3. Ad configuration of the panel - categories in show-categories.js

--- src/js/show-categories.js
+++ src/js/show-categories.js
@@ -1,14 +1,34 @@
 $(document).ready(function() {
   $('.select-option').on('click', function(event) {
-   
+
     var selectSection = $(this).data('section');
     var elContentOption = $('.' + selectSection);
-    
+
     $(this).addClass('active');
     $('.select-option').not(this).removeClass('active');
-    
+
     elContentOption.addClass('active');
     $('.info-selected-option').not(elContentOption).removeClass('active');
 
   });
+});
+
+window.addEventListener('scroll', function (){
+  var windowHeight = window.innerHeight;
+  var homeIsoPos = document.getElementsByClassName('home-iso-logo')[0].getBoundingClientRect().top;
+  var homeIso = document.querySelector('.home-iso-logo');
+
+  var homeDesignPos = document.getElementsByClassName('home-design-img-container')[0].getBoundingClientRect().top;
+  var homeDesign = document.querySelector('.home-design-img-container');
+  if (homeIsoPos < windowHeight) {
+    homeIso.classList.add('pulse');
+  } else {
+    homeIso.classList.remove('pulse');
+  }
+
+  if (homeDesignPos < windowHeight) {
+    homeDesign.classList.add('fadeInDown');
+  } else {
+    homeDesign.classList.remove('fadeInDown');
+  } 
 });
