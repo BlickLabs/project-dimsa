9344835b6529fac6b690ecfd8a0fe940323f6dab
Raúl Marty <santor3medio@gmail.com>
2018-02-09 21:12:12 -0600

    #3. Add function to close - open navbar - dropdown in mobile version to navbar-dropdown.js

--- src/js/navbar-dropdown.js
+++ src/js/navbar-dropdown.js
@@ -2,6 +2,10 @@ function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
 }
 
+function hamburgerDropDown() {
+    document.getElementById("hambugerDropdown").classList.toggle("show");
+}
+
 // Close the dropdown if the user clicks outside of it
 window.onclick = function(event) {
   if (!event.target.matches('.dropbtn')) {
