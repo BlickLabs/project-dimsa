ed06e987b2aa3eee5d433cb0174daea78069bca8
Raúl Marty <santor3medio@gmail.com>
2018-02-15 15:41:10 -0600

    #11. Add script to include carousel of banner selector of view servicios in mobile version to services-carousel.js

---
+++ src/js/services-carousel.js
@@ -0,0 +1,11 @@
+var servicesCarousel = $('.services-carousel');
+
+servicesCarousel.owlCarousel ({
+  loop: false,
+  margin: 10,
+  nav: false,
+  autoWidth: true,
+  items: 4,
+  dots: true,
+  center: true
+});
